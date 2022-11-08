eda1e359c7b01948ab8edb5058dbd7b3db50b809
RahulR100 <rahulrao@email.com>
2022-11-08 08:05:51 -0800

    update timezone

--- aplet/templatetags/custom.py
+++ aplet/templatetags/custom.py
@@ -1,11 +1,12 @@
 from django import template
 from dateutil import parser
+from datetime import timedelta
 
 register = template.Library()
 
 @register.filter(name='toDate')
 def toDate(value):
-    return parser.parse(value)
+    return parser.parse(value) - timedelta(hours=5)
 
 @register.filter(name='candidateVotePercent')
 def candidateVotePercent(value, arg):
